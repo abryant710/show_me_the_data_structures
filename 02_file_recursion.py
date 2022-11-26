5ec9b0a6ac1134b2387a4c3cfb4b37b59192bdce
Alex Bryant <alexbryant710@gmail.com>
2022-11-26 17:22:49 +1100

    Second task complete with all test cases

--- 02_file_recursion.py
+++ 02_file_recursion.py
@@ -52,6 +52,8 @@ print (os.path.isfile("./ex.py"))
 print ("./ex.py".endswith(".py"))
 """
 
+import os  # import os module
+
 
 def find_files(suffix, path):
     """
@@ -69,14 +71,80 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    if not suffix or not path:
+        print("Inputs suffix and/or path cannot be None")
+        return None
+    if not os.path.exists(path):
+        print("Path is not valid")
+        return None
+
+    files = []  # create empty list to store files
+    if os.path.isfile(path):  # check if path is a file
+        if path.endswith(suffix):  # check if file ends with suffix
+            files.append(path)  # append file to list
+    elif os.path.isdir(path):  # check if path is a directory
+        for file in os.listdir(path):  # iterate through files in directory
+            files += find_files(suffix, os.path.join(path, file))  # recursively call function
+
+    return files  # return list of files
 
 
 # Add your own test cases: include at least three test cases
 # and two of them must include edge cases, such as null, empty or very large values
 
+# Find cases of .c file extension
 # Test Case 1
+def test_01():
+    files = find_files(".c", "./testdir")
+    assert len(files) == 4, "Test Case 1 Failed"
+    assert "./testdir/subdir1/a.c" in files, "Test Case 1 Failed"
+    assert "./testdir/subdir3/subsubdir1/b.c" in files, "Test Case 1 Failed"
+    assert "./testdir/t1.c" in files, "Test Case 1 Failed"
+    assert "./testdir/subdir5/a.c" in files, "Test Case 1 Failed"
+    print("Test Case 1 Passed")
 
+
+test_01()
+
+# Find cases of .c file extension in a subdirectory
 # Test Case 2
+def test_02():
+    files = find_files(".c", "./testdir/subdir1")
+    assert len(files) == 1, "Test Case 2 Failed"
+    assert "./testdir/subdir1/a.c" in files, "Test Case 2 Failed"
+    print("Test Case 2 Passed")
 
+
+test_02()
+
+# Testing case where None is passed as suffix
 # Test Case 3
+def test_03():
+    files = find_files(None, "./testdir")
+    assert files == None, "Test Case 3 Failed"
+    print("Test Case 3 Passed")
+
+
+test_03()
+
+
+# Testing case where None is passed as path
+# Test Case 4
+def test_04():
+    files = find_files(".c", None)
+    assert files == None, "Test Case 4 Failed"
+    print("Test Case 4 Passed")
+
+
+test_04()
+
+
+# Testing case where a non-existent path is passed
+# Test Case 5
+def test_05():
+    files = find_files(".c", "./testdir/invalid")
+    assert files == None, "Test Case 5 Failed"
+    print("Test Case 5 Passed")
+
+
+test_05()
